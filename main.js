7d695699b67326de90d4f6890820781fe4cfaae9
Sushil Ganesh <sganesh4@ncsu.edu>
2015-10-22 01:01:51 -0400

    Updated main.js with testcases

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var esprima = require("esprima");
+var Chance = require("chance")
+var chance = new Chance()
 var options = {
 	tokens: true,
 	tolerant: true,
@@ -28,13 +30,6 @@ function main() {
 
 var engine = Random.engines.mt19937().autoSeed();
 
-function createConcreteIntegerValue(greaterThan, constraintValue) {
-	if (greaterThan)
-		return Random.integer(constraintValue, constraintValue + 10)(engine);
-	else
-		return Random.integer(constraintValue - 10, constraintValue)(engine);
-}
-
 function Constraint(properties) {
 	this.ident = properties.ident;
 	this.expression = properties.expression;
@@ -53,28 +48,6 @@ function fakeDemo() {
 }
 fakeDemo()
 var functionConstraints = {}
-
-var mockFileLibrary = {
-	pathExists: {
-		'path/fileExists': {
-			'file1': 'some file1 stuff here',
-			'file2': 'some file2 stuff here'
-		}
-	},
-	fileWithContent: {
-		pathContent: {
-			file1: 'text content',
-		}
-	},
-	emptyDir: {
-		'path/fileExists': {}
-	},
-	fileWithoutContent: {
-		pathContent: {
-			file1: '',
-		}
-	}
-};
 
 function generateTestCases() {
 
@@ -124,20 +97,7 @@ function generateTestCases() {
 		//var args = Object.keys(params).map( function(k) {return (typeof params[k] == 'string')?"'"+params[k]+"'":params[k] }).join(",");
 		for (var j = 0; j < argsList.length; j++) {
 			var args = argsList[j]
-			if (pathExists || fileWithContent) {
-				var splitArgs = args.split(',')
-				if (splitArgs[1] == '\'path/fileExists\'' || splitArgs[1] == '\'\'') continue;
-				for (var l = 0; l < 3; l++) {
-					content += generateMockFsTestCases(pathExists, fileWithContent, funcName, args);
-					// Bonus...generate constraint variations test cases....
-					content += generateMockFsTestCases(!pathExists, fileWithContent, funcName, args);
-					content += generateMockFsTestCases(pathExists, !fileWithContent, funcName, args);
-					content += generateMockFsTestCases(!pathExists, !fileWithContent, funcName, args);
-				}
-			} else {
-				// Emit simple test case.
-				content += "subject.{0}({1});\n".format(funcName, args);
-			}
+			content += "subject.{0}({1});\n".format(funcName, args);
 		}
 
 	}
@@ -145,34 +105,6 @@ function generateTestCases() {
 
 	fs.writeFileSync('test.js', content, "utf8");
 
-}
-
-function generateMockFsTestCases(pathExists, fileWithContent, funcName, args) {
-	var testCase = "";
-	// Build mock file system based on constraints.
-	var mergedFS = {};
-	if (pathExists) {
-		for (var attrname in mockFileLibrary.pathExists) {
-			mergedFS[attrname] = (Math.random() > 0.5) ? mockFileLibrary.pathExists[attrname] : mockFileLibrary.emptyDir[attrname];
-		}
-	}
-	if (fileWithContent) {
-		for (var attrname in mockFileLibrary.fileWithContent) {
-			mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname];
-		}
-	} else {
-		for (var attrname in mockFileLibrary.fileWithoutContent) {
-			mergedFS[attrname] = mockFileLibrary.fileWithoutContent[attrname];
-		}
-	}
-	testCase +=
-		"mock(" +
-		JSON.stringify(mergedFS) +
-		");\n";
-
-	testCase += "\tsubject.{0}({1});\n".format(funcName, args);
-	testCase += "mock.restore();\n";
-	return testCase;
 }
 
 function constraints(filePath) {
@@ -206,6 +138,7 @@ function constraints(filePath) {
 						var incorrect = "\"\""
 						switch (child.operator) {
 							case '==': //do nothing to rightHand
+							case '===':incorrect  = chance.string()
 								break
 							case '>':
 								incorrect = rightHand - 10
@@ -237,23 +170,6 @@ function constraints(filePath) {
 								operator: child.operator,
 								expression: expression
 							}));
-					} else if (child.left.type == 'Identifier' &&
-						params.length == 1 &&
-						params[0] == 'phoneNumber') {
-						var expression = buf.substring(child.range[0], child.range[1]);
-						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
-						if (child.right.type == 'Literal') {
-							rightHand = child.right.value
-						}
-						functionConstraints[funcName].constraints.push(
-							new Constraint({
-								ident: params[0],
-								value: '\''+rightHand+faker.phone.phoneNumberFormat().substring(3)+'\'',
-								funcName: funcName,
-								kind: "phoneNumber",
-								operator: child.operator,
-								expression: expression
-							}));
 					}
 				}
 				if (child.type == "CallExpression" &&
@@ -266,20 +182,6 @@ function constraints(filePath) {
 							value: child.arguments[0].raw,
 							funcName: funcName,
 							kind: "string",
-							operator: child.operator,
-							expression: expression
-						}));
-				}
-				if (child.type == "CallExpression" &&
-					child.arguments &&
-					child.callee.name == "format") {
-					functionConstraints[funcName].constraints.push(
-						new Constraint({
-							ident: child.arguments[0].name,
-							// A fake path to a file
-							value: '\'' + faker.phone.phoneNumberFormat() + '\'',
-							funcName: funcName,
-							kind: "phoneNumber",
 							operator: child.operator,
 							expression: expression
 						}));
