baf5976bc517a000d6ce774a2b63f9ec98c36cc3
Abidaan <abidaan18@gmail.com>
2015-10-23 19:30:53 -0400

    Updated testCoverage script

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -3,5 +3,5 @@ node main.js
 node_modules/.bin/istanbul cover test.js
 coverage=$(node_modules/.bin/istanbul cover test.js | grep 'Branch' | cut -d: -f 2 | cut -d. -f 1)
 if [ $coverage -lt 75 ]
-	exit 1
+	then exit 1
 fi
