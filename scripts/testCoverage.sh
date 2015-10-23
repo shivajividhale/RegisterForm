16ebbf90bafbf1234d24305d77fd36fda4577570
Abidaan <abidaan18@gmail.com>
2015-10-23 15:46:34 -0400

    Modified testCoverage script

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /Users/Abidaan/Documents/Term\ -\ 3/CSC\ 591/Project/RegisterForm
 node main.js
 coverage=$(node_modules/.bin/istanbul cover test.js | grep 'Branch' | cut -d: -f 2 | cut -d% -f 1)
 echo $coverage
