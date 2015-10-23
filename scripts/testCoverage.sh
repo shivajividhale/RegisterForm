eccb776ccd165febf884be574bdea26d416c3b7a
Abidaan <abidaan18@gmail.com>
2015-10-23 16:31:02 -0400

    Modified testCoverage script

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 node main.js
-coverage=$(node_modules/.bin/istanbul cover test.js | grep 'Branch' | cut -d: -f 2 | cut -d% -f 1)
-echo $coverage
-if [ "$coverage" < "$90" ]
-then exit 1
+coverage=`node_modules/.bin/istanbul cover test.js | grep 'Branch' | cut -d: -f 2 | cut -d. -f 1`
+if [ $coverage -lt 90 ]
+	exit 1
 fi
