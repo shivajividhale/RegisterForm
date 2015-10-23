a9e1739a3f594c7d0252e509820d6fc13a84aa9f
Abidaan <abidaan18@gmail.com>
2015-10-23 16:45:33 -0400

    Modified testCoverage build script

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 node main.js
-coverage=`node_modules/.bin/istanbul cover test.js | grep 'Branch' | cut -d: -f 2 | cut -d. -f 1`
+node_modules/.bin/istanbul cover test.js
+coverage=$(node_modules/.bin/istanbul cover test.js | grep 'Branch' | cut -d: -f 2 | cut -d. -f 1)
 if [ $coverage -lt 90 ]
 	exit 1
 fi
